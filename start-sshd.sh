6c122ae5f4164a1204625e2060210cb6348bbcc9
Teubel György <tgyurci@gmail.com>
2021-11-09 07:35:11 +0100

    Start script fixes

--- start-sshd.sh
+++ start-sshd.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-: "${SSHD_CONFIG_DIR:-"/etc/ssh"}"
-: "${SSHD_HOST_KEY_ALGOS:-"rsa ecdsa ed25519"}"
+: "${SSHD_CONFIG_DIR:="/etc/ssh"}"
+: "${SSHD_HOST_KEY_ALGOS:="rsa ecdsa ed25519"}"
+: "${SSHD_HOST_KEY_COMMENT:=""}"
 
 for algo in $SSHD_HOST_KEY_ALGOS; do
 	host_key_file="/etc/ssh/ssh_host_${algo}_key"
 
 	[ -f "$host_key_file" ] || {
 		echo "Generating host key: $host_key_file"
-		ssh-keygen -q -f "$host_key_file" -t "$algo" -N ''
+		ssh-keygen -q -f "$host_key_file" -t "$algo" ${SSHD_HOST_KEY_COMMENT:+-C "${SSHD_HOST_KEY_COMMENT}"} -N ''
+		ssh-keygen -l -f "$host_key_file"
 	}
 done
 
